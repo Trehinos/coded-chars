d5d2949a4dbc96287d104016c2f40578d898f5d8
Sébastien Geldreich <trehinos@gmail.com>
2024-08-28 22:31:16 +0200

    Rename crate and enhance documentation
    Renamed crate from "ecma-48" to "coded-chars" in Cargo.toml and Cargo.lock. Added detailed documentation for `insert_char`, `insert_line`, and `delete_char` functions in `src/control/rendition.rs`. Enhanced crate documentation and example usage in `src/lib.rs`.

--- src/control/rendition.rs
+++ src/control/rendition.rs
@@ -1,14 +1,84 @@
 use std::fmt::{Display, Formatter};
 use crate::control::ControlSequence;
 
+/// # ICH - Insert character
+/// 
+/// If the DEVICE COMPONENT SELECT MODE (DCSM) is set to PRESENTATION, ICH is used to
+/// prepare the insertion of n characters, by putting into the erased state the active presentation position and,
+/// depending on the setting of the CHARACTER EDITING MODE (HEM), the n-1 preceding or following
+/// character positions in the presentation component, where n equals the value of Pn. The previous contents
+/// of the active presentation position and an adjacent string of character positions are shifted away from the
+/// active presentation position. The contents of n character positions at the other end of the shifted part are
+/// removed. The active presentation position is moved to the line home position in the active line. The line
+/// home position is established by the parameter value of SET LINE HOME (SLH).
+///
+/// The extent of the shifted part is established by SELECT EDITING EXTENT (SEE).
+///
+/// The effect of ICH on the start or end of a selected area, the start or end of a qualified area, or a
+/// tabulation stop in the shifted part, is not defined by this Standard.
+///
+/// If the DEVICE COMPONENT SELECT MODE (DCSM) is set to DATA, ICH is used to prepare the
+/// insertion of n characters, by putting into the erased state the active data position and, depending on the
+/// setting of the CHARACTER EDITING MODE (HEM), the n-1 preceding or following character
+/// positions in the data component, where n equals the value of Pn. The previous contents of the active data
+/// position and an adjacent string of character positions are shifted away from the active data position. The
+/// contents of n character positions at the other end of the shifted part are removed. The active data
+/// position is moved to the line home position in the active line. The line home position is established by
+/// the parameter value of SET LINE HOME (SLH).
 pub fn insert_char(n: usize) -> ControlSequence {
     ControlSequence::new(&[&n.to_string()], "@")
 }
 
+/// # IL - Insert line
+/// 
+/// If the DEVICE COMPONENT SELECT MODE (DCSM) is set to PRESENTATION, IL is used to
+/// prepare the insertion of n lines, by putting into the erased state in the presentation component the active
+/// line (the line that contains the active presentation position) and, depending on the setting of the LINE
+/// EDITING MODE (VEM), the n-1 preceding or following lines, where n equals the value of Pn. The
+/// previous contents of the active line and of adjacent lines are shifted away from the active line. The
+/// contents of n lines at the other end of the shifted part are removed. The active presentation position is
+/// moved to the line home position in the active line. The line home position is established by the
+/// parameter value of SET LINE HOME (SLH).
+///
+/// The extent of the shifted part is established by SELECT EDITING EXTENT (SEE).
+///
+/// Any occurrences of the start or end of a selected area, the start or end of a qualified area, or a tabulation
+/// stop in the shifted part, are also shifted.
+///
+/// If the TABULATION STOP MODE (TSM) is set to SINGLE, character tabulation stops are cleared in
+/// the lines that are put into the erased state.
+///
+/// If the DEVICE COMPONENT SELECT MODE (DCSM) is set to DATA, IL is used to prepare the
+/// insertion of n lines, by putting into the erased state in the data component the active line (the line that
+/// contains the active data position) and, depending on the setting of the LINE EDITING MODE (VEM),
+/// the n-1 preceding or following lines, where n equals the value of Pn. The previous contents of the active
+/// line and of adjacent lines are shifted away from the active line. The contents of n lines at the other end
+/// of the shifted part are removed. The active data position is moved to the line home position in the active
+/// line. The line home position is established by the parameter value of SET LINE HOME (SLH).
 pub fn insert_line(n: usize) -> ControlSequence {
     ControlSequence::new(&[&n.to_string()], "K")
 }
 
+/// # DCH - Delete character
+/// 
+/// If the DEVICE COMPONENT SELECT MODE (DCSM) is set to PRESENTATION, DCH causes the
+/// contents of the active presentation position and, depending on the setting of the CHARACTER
+/// EDITING MODE (HEM), the contents of the n-1 preceding or following character positions to be
+/// removed from the presentation component, where n equals the value of Pn. The resulting gap is closed
+/// by shifting the contents of the adjacent character positions towards the active presentation position. At
+/// the other end of the shifted part, n character positions are put into the erased state.
+///
+/// The extent of the shifted part is established by SELECT EDITING EXTENT (SEE).
+///
+/// The effect of DCH on the start or end of a selected area, the start or end of a qualified area, or a
+/// tabulation stop in the shifted part is not defined by this Standard.
+///
+/// If the DEVICE COMPONENT SELECT MODE (DCSM) is set to DATA, DCH causes the contents of
+/// the active data position and, depending on the setting of the CHARACTER EDITING MODE (HEM),
+/// the contents of the n-1 preceding or following character positions to be removed from the data
+/// component, where n equals the value of Pn. The resulting gap is closed by shifting the contents of the
+/// adjacent character positions towards the active data position. At the other end of the shifted part, n
+/// character positions are put into the erased state.
 pub fn delete_char(n: usize) -> ControlSequence {
     ControlSequence::new(&[&n.to_string()], "P")
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,30 @@
-//! This crate implements [ecma-48](https://ecma-international.org/publications-and-standards/standards/ecma-48/) for rust.
+//! This crate implements ECMA standards for coded characters in rust.
 //!
 //! Various constructions are provided to easily add control character and sequences inside text.
 //!
-//! ### Example
-//! ```
-//! use ecma_48::control::rendition::select;
+//! This crate is compatible with ANSI terminals.
 //!
-//! println!("Hello {}{}{} !", select().fg_red().bold().underline(), "World", select().default())
+//! ### Standards implemented
+//! - [ecma-35](https://ecma-international.org/publications-and-standards/standards/ecma-35/) - Character Code Structure and Extension Techniques
+//! - [ecma-43](https://ecma-international.org/publications-and-standards/standards/ecma-43/) - 8-Bit Coded Character Set Structure and Rules
+//! - [ecma-48](https://ecma-international.org/publications-and-standards/standards/ecma-48/) - Control Functions for Coded Character Sets
+//!
+//! ### Example : format text in an ANSI terminal
+//! ```
+//! use coded_chars::control::rendition::{format_str, next_page, select};
+//!
+//! // Direct format
+//! println!("Hello {}{}{} !", select().fg_red().bold().underline(), "World", select().default());
+//!
+//! // Clear screen
+//! next_page(1);
+//!
+//! // Using format_str
+//! let formatted = format_str(
+//!     "World",
+//!     select().fg_red().bold().underline()
+//!  );
+//! println!("Hello {} !", formatted);
 //! ```
 
 pub mod characters;
@@ -16,13 +34,21 @@ pub mod control;
 
 #[cfg(test)]
 mod tests {
-    use crate::control::rendition::{format_str, select};
+    use crate::control::rendition::{format_str, next_page, select};
 
     #[test]
     fn test() {
+
+        // Direct format
+        println!("Hello {}{}{} !", select().fg_red().bold().underline(), "World", select().default());
+
+        // Clear screen
+        next_page(1);
+
+        // Using format_str
         let formatted = format_str(
-            "World", 
-            select().fg_black().bg_yellow().bold().underline()
+            "World",
+            select().fg_red().bold().underline()
         );
         println!("Hello {} !", formatted);
     }
