8998d6f57b9ec8023d24de76d240ea4a4c3193cf
Sébastien Geldreich <trehinos@gmail.com>
2024-08-31 14:08:31 +0200

    Update set_position function and bump version to 0.1.4
    Changed control sequence identifier in set_position function from " i" to "H" for improved clarity and adherence to standards. Also updated Cargo.toml and Cargo.lock to reflect the version bump to 0.1.4.

--- src/control/cursor.rs
+++ src/control/cursor.rs
@@ -6,5 +6,5 @@ use crate::control::ControlSequence;
 /// position according to the line progression and to the m-th character position according to the character
 /// path, where n equals the value of `l` and m equals the value of `c`.
 pub fn set_position(l: usize, c: usize) -> ControlSequence {
-    ControlSequence::new(&[&l.to_string(), &c.to_string()], " i")
+    ControlSequence::new(&[&l.to_string(), &c.to_string()], "H")
 }
